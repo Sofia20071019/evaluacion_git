f378452064f419ce0976b13f42a2d81dcd0fa32f
Sofia20071019 <sofiapira2020@gmail.com>
2026-02-13 17:20:59 -0500

    Implementa_promedio

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 def analisar(lista):
-    pass  
+    menor = min(lista)
+    promedio = sum(lista) / len(lista)
+    return{
+        "menor": menor,
+        "promedio": promedio
+    }
 
 if  __name__ == "__main__":
     numeros = [4, 8, 2, 10]
     resultado = analisar(numeros)
     print(resultado)
-    
